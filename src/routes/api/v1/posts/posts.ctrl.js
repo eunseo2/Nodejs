e8994bc2ccc7e0600b9bcee224b3447714d2a58d
eunseo2 <20180972@sungshin.ac.kr>
2021-03-17 13:24:38 +0900

    prepares posts api

---
+++ src/routes/api/v1/posts/posts.ctrl.js
@@ -0,0 +1,21 @@
+//write, list, read, update, remove
+
+exports.write = (req, res) => {
+  res.send('write');
+};
+
+exports.list = (req, res) => {
+  res.send('list');
+};
+
+exports.read = (req, res) => {
+  res.send('read');
+};
+
+exports.update = (req, res) => {
+  res.send('update');
+};
+
+exports.remove = (req, res) => {
+  res.send('remove');
+};
